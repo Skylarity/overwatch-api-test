6b8c340e36f0638d5aa51937e388ac57ae9172a8
Skyler Rexroad <skyrex1095@gmail.com>
2017-07-26 21:23:31 -0600

    Initial setup

---
+++ src/components/Hello.vue
@@ -0,0 +1,38 @@
+<template>
+	<h1>Overwatch API Test</h1>
+	<pre>
+		{{ formattedData }}
+	</pre>
+</template>
+
+<script>
+export default {
+	name: 'hello',
+	data () {
+		return {
+			data: {}
+		}
+	},
+	mounted: function() {
+		this.getBlob()
+	},
+	methods: {
+		getBlob: function() {
+			this.$http.get('https://owapi.net/api/v3/u/Skylarity-1114/blob')
+				.then(function(result) {
+					this.data = result.body
+				})
+		}
+	},
+	computed: {
+		formattedData: function() {
+			return JSON.stringify(data)
+		}
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
